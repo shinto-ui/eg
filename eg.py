4d974888accc45733a68381f8ccff8b508c3c4fe
shinto <shintoclimson2005@gmail.com>
2026-06-19 14:36:09 +0530

    master

---
+++ eg.py
@@ -0,0 +1,12 @@
+import random
+
+while True:
+    input("Press Enter to roll the dice...")
+    dice = random.randint(1, 6)
+    print("You rolled:", dice)
+
+    choice = input("Roll again? (y/n): ")
+    if choice.lower() != 'y':
+        break
+
+print("Game Over!")
